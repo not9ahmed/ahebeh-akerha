606c6c90b01f73fc0b89014e78138e06d288ba0a
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-10-16 10:13:38 +0300

    Added views

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const express = require('express')
 
 
-const app = new express()
+const app = express()
 
 const PORT = 3000
+
+const ejsLayouts = require('express-ejs-layouts')
 
 
 // MIDDLEWARE
 app.set('view engine', 'ejs')
-
+app.use(ejsLayouts)
 
 
 // ROUTES
 
 app.get('/', (req, res) => {
-    res.send('YOOO')
+    res.render('home.ejs')
 })
 
 
+// Favorite animals
+app.get('/animals', (req, res) => {
+
+    const context ={
+        animals: ['house cats' ,'arctic monkeys', 'fluffy puppies']
+    }
+
+    res.render('animals.ejs', context)
+})
 
 
-
-// APP Listen
+// APP Listener
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
